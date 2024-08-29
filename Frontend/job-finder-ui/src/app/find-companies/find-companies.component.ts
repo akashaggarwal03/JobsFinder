83f8b99106b0fd422fb029a9ccf0c5274342c499
akash <aakash.aggarwal03@gmail.com>
2024-08-29 23:43:14 +0530

    Fixed css of feature page.

--- Frontend/job-finder-ui/src/app/find-companies/find-companies.component.ts
+++ Frontend/job-finder-ui/src/app/find-companies/find-companies.component.ts
@@ -1,44 +1,49 @@
-import { Component, EnvironmentInjector } from '@angular/core';
-import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import {MatButtonModule} from '@angular/material/button';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { JobFinderService } from '../Service/job-finder.service';
-import {MatTableModule} from '@angular/material/table';
 import { CompanyTableComponent } from '../company-table/company-table.component';
 import { TIME_RANGES } from '../Utils/DateRanges';
 import { CommonModule } from '@angular/common';
 import { OverlayLoadingComponent } from '../Utils/overlay-loading/overlay-loading.component';
+import { DropdownModule } from 'primeng/dropdown';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
 @Component({
-  selector: 'find-Companies',
+  selector: 'find-companies',
   standalone: true,
-  imports: [MatFormFieldModule,
-    MatSelectModule, FormsModule, ReactiveFormsModule, MatButtonModule, MatTableModule,
-    CompanyTableComponent, CommonModule, OverlayLoadingComponent],
+  imports: [
+    ReactiveFormsModule, CommonModule, CompanyTableComponent, OverlayLoadingComponent,
+    DropdownModule, ButtonModule, CardModule
+  ],
   templateUrl: './find-companies.component.html',
-  styleUrl: './find-companies.component.scss'
+  styleUrls: ['./find-companies.component.scss']
 })
-export class FindCompaniesComponent {
-
-  constructor(private jobFinderService: JobFinderService) {}
-
-  options = new FormControl('');
-  optionList = TIME_RANGES;
+export class FindCompaniesComponent implements OnInit {
+  formGroup!: FormGroup;
+  timeRanges = TIME_RANGES;
   companiesData: any[] = [];
   isLoading: boolean = false;
 
+  constructor(private jobFinderService: JobFinderService) {}
 
+  ngOnInit() {
+    this.formGroup = new FormGroup({
+      selectedTimeRange: new FormControl(null)
+    });
+  }
 
   onSearch() {
-    console.log('Selected option:', this.options.value);
+    const selectedRange = this.formGroup?.get('selectedTimeRange')?.value;
+    console.log('Selected option:', selectedRange);
     this.isLoading = true;
     this.companiesData = [];
-    // Implement your search logic here
-    this.jobFinderService.getJobs(this.options.value || "").subscribe(response => {
+
+    this.jobFinderService.getJobs(selectedRange?.value || "").subscribe(response => {
       this.companiesData = response;
       console.log('Job listings:', response);
-      this.isLoading =false;
+      this.isLoading = false;
     });
   }
 }
+
