83e8a73967aa08453c94c69bd9a6e91a3c9f3add
akash <aakash.aggarwal03@gmail.com>
2024-12-16 23:21:12 +0530

    Few UI changes.

--- Frontend/job-finder-ui/src/app/Utils/companies.ts
+++ Frontend/job-finder-ui/src/app/Utils/companies.ts
@@ -1,1 +1,2 @@
-export const COMPANIES = ["Google", "Microsoft", "Amazon", "Adobe", "Coinbase"];
+export const COMPANIES = ["Google", "Microsoft", "Amazon", "Adobe", "Coinbase", "Meta", "ServiceNow", "Flipkart", "Uber",
+"Goldman Sachs","Salesforce", ];
