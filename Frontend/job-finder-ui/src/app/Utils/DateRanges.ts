65116e3e329657313902bfbff6bb963ab6855bb0
akash <aakash.aggarwal03@gmail.com>
2025-01-09 00:29:56 +0530

    Some config changes.

--- Frontend/job-finder-ui/src/app/Utils/DateRanges.ts
+++ Frontend/job-finder-ui/src/app/Utils/DateRanges.ts
@@ -6,6 +6,5 @@ export interface TimeRange {
 export const TIME_RANGES: TimeRange[] = [
   { label: 'LAST WEEK', value: 'LAST_WEEK' },
   { label: 'LAST MONTH', value: 'LAST_MONTH' },
-  { label: 'LAST SIX MONTHS', value: 'LAST_SIX_MONTH' },
-  { label: 'ANYTIME', value: 'ANYTIME' }
+  { label: 'LAST SIX MONTHS', value: 'LAST_SIX_MONTH' }
 ];
